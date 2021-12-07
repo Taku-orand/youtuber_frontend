e5a1215a86df0e1ce1ad278db8e21372caebebc3
Takuya <uoastudent182@gmail.com>
2021-12-07 13:20:02 +0900

    envファイル追加
    環境変数を使用するため

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -9,7 +9,7 @@ const options = {
 
 const client = applyCaseMiddleware(
   axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: process.env.REACT_APP_BACKEND_URL,
   }),
   options
 );
